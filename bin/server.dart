00e5109e8fc800b3d1493789b5be2ed1186a1570
Eliseu Codinhoto <zeucxb@gmail.com>
2022-03-27 15:01:20 -0300

    change service

--- bin/server.dart
+++ bin/server.dart
@@ -4,6 +4,8 @@ import 'package:args/args.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
+import 'service.dart';
+
 // For Google Cloud Run, set _hostname to '0.0.0.0'.
 const _hostname = 'localhost';
 
@@ -22,13 +24,12 @@ void main(List<String> args) async {
     return;
   }
 
+  final service = Service();
+
   var handler = const shelf.Pipeline()
       .addMiddleware(shelf.logRequests())
-      .addHandler(_echoRequest);
+      .addHandler(service.handler);
 
   var server = await io.serve(handler, _hostname, port);
   print('Serving at http://${server.address.host}:${server.port}');
 }
-
-shelf.Response _echoRequest(shelf.Request request) =>
-    shelf.Response.ok('Request for "${request.url}"');
